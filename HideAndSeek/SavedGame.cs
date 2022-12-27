3efbb90535676184449a4614d00974d77e397704
lexxA87 <alex.o.nikonov@gmail.com>
2022-12-27 20:52:57 +0400

    Added SavedGame class

---
+++ HideAndSeek/SavedGame.cs
@@ -0,0 +1,10 @@
+﻿namespace HideAndSeek
+{
+    public class SavedGame
+    {
+        public string PlayerLocation { get; set; }
+        public Dictionary<string, string> OpponentLocations { get; set; }
+        public List<string> FoundOpponents { get; set; }
+        public int MoveNumber { get; set; }
+    }
+}
